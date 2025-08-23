d4f0a05b5c04919413aaded36da1cd4006150cb8
SenbaLenin <senbagaml@gmail.com>
2025-08-23 19:53:49 -0400

    Add files via upload

---
+++ recipes/LightBright.js
@@ -0,0 +1,9 @@
+export const LightBright = [
+  { title: 'Payasam', detailsFile: './recipes/New/Payasam.html' },
+  { title: 'Sweetpotato Wrap', detailsFile: './recipes/New/Sweetpotato Wrap.html' },
+ { title: 'Rice Kanji', detailsFile: './recipes/New/Rice Kanji.html' },
+ { title: 'Upma', detailsFile: './recipes/New/Upma.html' },
+ { title: 'Mango Pickle', detailsFile: './recipes/New/Mango Pickle.html' },
+  { title: 'Lemon Pickle', detailsFile: './recipes/New/LemonPickle.html' },
+ { title: 'Spicy Tomoto Pickle', detailsFile: './recipes/New/Spicy Tomoto pickle.html' },
+  ];
---
+++ recipes/RiceDelights.js
@@ -0,0 +1,13 @@
+export const RiceDelights = [
+   
+   { title: 'Sweet Pongale', detailsFile: './recipes/Sweets/sweetpongal.html' },
+   { title: 'Ven Pongal', detailsFile: './recipes/New/Ven Pongal.html' },
+   { title: 'Veg Pulav', detailsFile: './recipes/New/Veg Pulav.html' },
+   { title: 'Parupu Sadham', detailsFile: './recipes/New/Parupu Sadham.html' },
+   { title: 'Thatapayeru Sadham', detailsFile: './recipes/New/Thatapayeru Sadham.html' },
+   { title: 'Puli Sadham', detailsFile: './recipes/New/Puli Sadham.html' },
+   { title: 'Lemon Rice', detailsFile: './recipes/New/Lemon Rice.html' },
+   { title: 'Thayir Sadham', detailsFile: './recipes/New/Curd Rice.html' },
+   { title: 'Sochi', detailsFile: './recipes/New/Sochi.html' },
+
+];
