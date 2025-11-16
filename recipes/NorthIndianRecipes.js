94e85f76c4317e273fa31c17c4d5c8aea984ca2d
SenbaLenin <senbagaml@gmail.com>
2025-11-16 15:54:21 -0500

    Add Mashroom Tika Masala to North Indian recipes
    added new recipie

--- recipes/NorthIndianRecipes.js
+++ recipes/NorthIndianRecipes.js
@@ -3,9 +3,16 @@ export const NorthIndian = [
     id: 'ChanaMasala',
     title: 'Chana Masala',
     detailsFile: 'recipes/NorthIndian/ChanaMasala.html'
-  },{
+  },
+  {
     id: 'DalMakhani',
     title: 'DalMakhani',
     detailsFile: 'recipes/NorthIndian/DalMakhani.html'
+  },
+  {
+    id: 'Mashroomtika',
+    title: 'Mashroom Tika Masala',
+    detailsFile: 'recipes/NorthIndian/Mashroomtika.html'
   }
+
 ];
